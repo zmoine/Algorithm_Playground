b546a9ed0a009e21034c6bfd6153080fb3bfb1c7
Hermoine <zhermoine@gmail.com>
2025-04-06 01:16:19 +0800

    Added comments

--- src/SortingAlgorithms/BubbleSort/bubbleSort.cpp
+++ src/SortingAlgorithms/BubbleSort/bubbleSort.cpp
@@ -5,7 +5,7 @@ void BubbleUp::sort(int arr[], int n){
     for(int i=0; i<n-1; i++){
         //Compare adjacent elements
         for(int j=0; j<n-i-1; j++){
-            //Swap if they are out of order, ascending
+            //Swap if they are out of order in ascending order
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
@@ -21,7 +21,7 @@ void BubbleDown::sort(int arr[], int n){
         //Compare adjacent elements
         for(int j=0; j<n-i-1; j++){
             if(arr[j] < arr[j+1]){
-                //Swap if they are out of order, descending
+                //Swap if they are out of order in descending order
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
