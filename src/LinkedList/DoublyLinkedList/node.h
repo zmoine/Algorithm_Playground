0574f46abda9302ff3712318c6b348edbebf0305
Hermoine <zhermoine@gmail.com>
2025-04-10 01:37:04 +0800

    Added Doubly Linked List

--- src/LinkedList/DoublyLinkedList/node.h
+++ src/LinkedList/DoublyLinkedList/node.h
@@ -3,18 +3,12 @@
 //Struct for the node
 struct node {
     int element;
-    node* next;
+    node* next ;
     node* prev;
 
-    //Constructor 1
     node(int element) {
         this->element = element;
-    }
-
-    //Constructor 2
-    node(node* next, node* prev, int element) {
-        this->next = next;
-        this->prev = prev;
-        this->element = element;
+        next = nullptr;
+        prev = nullptr;
     }
 };
